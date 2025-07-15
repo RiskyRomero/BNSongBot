2e131170c4025f3c833ef998d613174bebfa5e9b
Ryumin <riskyromero@proton.me>
2025-07-15 09:15:34 +0800

    refactor(ping): include latency

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -3,6 +3,17 @@ use crate::{Context, Error};
 /// Pings the bot
 #[poise::command(slash_command, prefix_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("Ping!").await?;
+    let latency = &ctx.framework().shard_manager.runners.lock().await
+        [&ctx.serenity_context().shard_id]
+        .latency;
+    let content: String;
+
+    if let Some(ping) = latency {
+        content = format!("Pong! 🏓 My latency is `{:.2}ms`", ping.as_millis());
+    } else {
+        content = String::from("Pong! 🏓 Latency is unknown");
+    }
+
+    ctx.say(content).await?;
     Ok(())
 }
